29f688aefbdc0678d339f8a2befbd91cd6f8ff8d
Giuliano <giuilianogarcia2826@gmail.com>
2023-03-04 23:42:58 -0300

    add menuMobile module

--- src/index.js
+++ src/index.js
@@ -1,26 +1,14 @@
+import { showMenuMobile } from "./modules/menuMobile";
 import "./sass/style.scss";
 
-const body = document.body;
-const menubtn = document.querySelector('.menu-btn');
-const burger = document.querySelector('.menu-btn__burger');
-const menu = document.querySelector('.menu');
-const navMenu = document.querySelector('.nav-menu');
-const menuLink = document.querySelectorAll('.menu-nav__link');
+showMenuMobile();
 
-menubtn.addEventListener('click', function() {
-    menu.classList.toggle('open');
-    burger.classList.toggle('open');
-    body.classList.toggle('overflow-hidden');
-    navMenu.classList.toggle('open');
-});
 
-menuLink.forEach(link =>{
-    link.addEventListener('click', function(){
-        menu.classList.toggle('open');
-        burger.classList.toggle('open');
-        body.classList.toggle('overflow-hidden');
-      
-    });
-});
 
- 
+
+
+
+
+
+
+
---
+++ src/modules/menuMobile.js
@@ -0,0 +1,36 @@
+const mediaQueries = window.matchMedia("(max-width: 767px)");
+const body = document.body
+const menubtn = document.querySelector('.menu-btn');
+const burger = document.querySelector('.menu-btn__burger');
+const menu = document.querySelector('.menu');
+const menuLink = document.querySelectorAll('.menu-nav__link');
+
+export function showMenuMobile() {
+
+    //open/close menu on click button 
+    menubtn.addEventListener('click', function () {
+        menu.classList.toggle('open');
+        burger.classList.toggle('open');
+        body.classList.toggle('overflow-hidden');
+    });
+
+    //close menu on click link menu 
+    menuLink.forEach(link => {
+        link.addEventListener('click', function () {
+            menu.classList.toggle('open');
+            burger.classList.toggle('open');
+            body.classList.remove('overflow-hidden');
+        });
+    });
+
+    //hide/show scroll bar on screen resize
+    mediaQueries.addEventListener('change', function () {
+        const menuOpen = menu.classList.contains('open');
+        
+        if (!mediaQueries.matches) body.classList.remove('overflow-hidden');
+        
+        if (mediaQueries.matches && menuOpen)  body.classList.add('overflow-hidden');
+        
+    });
+};
+
