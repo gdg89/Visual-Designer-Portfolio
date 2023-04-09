f9fc80d18d83b8b22980d976c5634d2dc892f60f
Giuliano <giuilianogarcia2826@gmail.com>
2023-04-09 01:52:13 -0300

    Scroll reveal

--- src/modules/scrollReveal.js
+++ src/modules/scrollReveal.js
@@ -10,10 +10,10 @@ export function heroScroll(){
     ScrollReveal().reveal('.exp-item4', { delay: 1200 });
     ScrollReveal().reveal('.exp-item5', { delay: 1500 });
     ScrollReveal().reveal('.projects-header', { delay: 500 });
-    ScrollReveal().reveal('.project-1', { delay: 300 });
-    ScrollReveal().reveal('.project-2', { delay: 600 });
-    ScrollReveal().reveal('.project-3', { delay: 900 });
-    ScrollReveal().reveal('.project-4', { delay: 1200 });
+    ScrollReveal().reveal('.project-1', { delay: 800 });
+    ScrollReveal().reveal('.project-2', { delay: 1100 });
+    ScrollReveal().reveal('.project-3', { delay: 1400 });
+    ScrollReveal().reveal('.project-4', { delay: 1700 });
 
 }
 
