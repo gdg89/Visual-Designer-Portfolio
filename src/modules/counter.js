c3fa8d59b26818da00370ce236b54eb0f55f2d18
Giuliano <giuilianogarcia2826@gmail.com>
2023-06-15 21:18:55 -0300

    fix interval counter

--- src/modules/counter.js
+++ src/modules/counter.js
@@ -3,23 +3,23 @@ const el = document.querySelector('.exp-test');
 export function counter(el) {
    const elMaxValue = el.dataset.value;
    let elValue = 0;
-   let interval = 200;
+   let interval = elMaxValue > 50 ? 100 : 200;
 
    const counterInterval = setInterval(() => {
       showCounter(el, elValue);
       elValue++;
 
-      if(elValue > 20) interval = 1;
-      if(elValue > elMaxValue) clearInterval(counterInterval);
+      if (elValue > elMaxValue) clearInterval(counterInterval);
    }, interval);
 }
 
 
 function showCounter(el, value) {
-   if(value > 50 ){
+   if (value > 50) {
       el.innerHTML = value.toString().padStart(2, '0') + "+";
-   }else{
-      el.innerHTML= value.toString().padStart(2, '0');
+   } else {
+      el.innerHTML = value.toString().padStart(2, '0');
    }
-   
+
 }
+
